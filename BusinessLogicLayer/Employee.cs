bc6ee5e007ad22ce538a3392bfbd189cf3604c47
kalpana786 <52391133+kalpana786@users.noreply.github.com>
2021-10-26 15:42:35 +0530

    Add files via upload

---
+++ BusinessLogicLayer/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using DataAccessLayer;
+
+
+namespace BusinessLogicLayer
+{
+    public class Employee
+    {
+        DataAccessLayer.Employee objDalEmployee = new DataAccessLayer.Employee();
+        public int addEmplopyee(BusinessObject.Employee objBoEmployee)
+        {
+            int i = objDalEmployee.addEmplopyee(objBoEmployee);
+            return i;
+        }
+
+        public int deleteEmplopyee(BusinessObject.Employee objBoEmployee)
+        {
+            int i = objDalEmployee.deleteEmplopyee(objBoEmployee);
+            return i;
+            
+        }
+        public int updateEmplopyee(BusinessObject.Employee objBoEmployee)
+        {
+            int i = objDalEmployee.updateEmplopyee(objBoEmployee);
+            return i;
+            
+        }
+        public DataSet searchEmplopyee(BusinessObject.Employee objBoEmployee)
+        {
+             DataSet ds= objDalEmployee.searchEmplopyee(objBoEmployee);
+            return ds;
+            
+        }
+        public DataSet allEmplopyee(BusinessObject.Employee objBoEmployee)
+        {
+            DataSet ds = objDalEmployee.allEmplopyee(objBoEmployee);
+            return ds;
+
+        }
+    }
+}
