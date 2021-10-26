0c4065181132b6714658686379184183d6ef91d9
kalpana786 <52391133+kalpana786@users.noreply.github.com>
2021-10-26 15:39:23 +0530

    Add files via upload

---
+++ BusinessObject/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BusinessObject
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
